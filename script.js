0d4bd8c2bea97fc7d6f58d0c17e57c43b95d2472
TGOLD <thomaswgold90@gmail.com>
2022-07-02 16:09:04 -0600

    init + rough draft html

---
+++ script.js
@@ -0,0 +1,12 @@
+//PSEUDO CODE
+//create start button that init timer funct and quiz questions funct
+//quiz question answer input must generate into next question
+//incorrect answer = -time
+//timer=0 or all questions answered --> game is over
+//input for initials and final score shown. can be saved.
+
+
+
+var timer = document.querySelector("#timer");
+var quizBox = document.querySelector("#quizBox");
+var startBtn = document.querySelector("#startbtn");
