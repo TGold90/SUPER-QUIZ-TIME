68586e979234d6f5c45ca09207c6160193864766
TGOLD <thomaswgold90@gmail.com>
2022-07-07 15:45:16 -0600

    tutor session: re-wrote currentQuestion to use forEach loop

--- script.js
+++ script.js
@@ -12,17 +12,17 @@
 var startBtn = document.querySelector("#startbtn"); //start button
 var timerElement = document.querySelector("#timer"); //timer content
 // var quizBox = document.querySelector("#quizBox");
-var answerSelect = document.querySelector(".answerbtn"); //all answer buttons - click event
-var answerOne = document.querySelector(".answer1");//indiv answer buttons
-var answerTwo = document.querySelector(".answer2");//indiv answer buttons
-var answerThree = document.querySelector(".answer3");//indiv answer buttons
-var answerFour = document.querySelector(".answer4");//indiv answer buttons
+var answerSelect = document.querySelector(".buttonbox"); //all answer buttons - click event
+// var answerOne = document.querySelector(".answer1");//indiv answer buttons
+// var answerTwo = document.querySelector(".answer2");//indiv answer buttons
+// var answerThree = document.querySelector(".answer3");//indiv answer buttons
+// var answerFour = document.querySelector(".answer4");//indiv answer buttons
 var questionContent = document.querySelector("#questioncontent");//p tag - should display the actual question
 // var gameOver = document.querySelector("#gameover");
 // var resetBtn = document.querySelector()
 var timer;
 var timeLeft = 60;
-var currentQuestionInd = "";
+var currentQuestionInd = 0;
 
 //function that will run when time runs out or all questions are answered
 // function endOfQuiz () {
@@ -44,73 +44,100 @@ startBtn.style.display = "none";
 //array of questions and answer info
  var questionSequence = [
      {question: 'What does HTML stand for?',
-        answerOne: "Hyper Tube Markup Language",
-        answerTwo: "Hyper Text Markup Language",
-        answerThree: "Hypno-Tron Meerkat Lounge",
-        answerFour: "Hold That Milk Log",
+        options:["Hyper Tube Markup Language", 
+        "Hyper Text Markup Language",
+        "Hypno-Tron Meerkat Lounge",
+        "Hold That Milk Log"], 
+        answer: "Hyper Text Markup Language"
  },
      {question: 'Which language is used primarily as styling for a webpage?',
-        answerOne: "CSS",
-        answerTwo: "HTML",
-        answerThree: "C++",
-        answerFour: "Javascript",
+     options:["Hyper Tube Markup Language", 
+     "Hyper Text Markup Language",
+     "Hypno-Tron Meerkat Lounge",
+     "Hold That Milk Log"], 
+     answer:"Hold That Milk Log"
  },
      {question: 'Which of the following would be considered an \'array method\'?',
-        answerOne: "right",
-        answerTwo: "wrong",
-        answerThree: "wrong",
-        answerFour: "wrong",
+     options:["Hyper Tube Markup Language", 
+     "Hyper Text Markup Language",
+     "Hypno-Tron Meerkat Lounge",
+     "Hold That Milk Log"], 
+     answer:"Hold That Milk Log"
  },
      {question: 'questionQuestion 4',
-        answerOne: "1d",
-        answerTwo: "2d",
-        answerThree: "3d",
-        answerFour: "4d",
+     options:["Hyper Tube Markup Language", 
+     "Hyper Text Markup Language",
+     "4c",
+     "test"], 
+     answer:"Hold That Milk Log"
  },
      {question: 'questions the fifth one',
-        answerOne: "an answer on ! 5",
-        answerTwo: "5b",
-        answerThree: "5c",
-        answerFour: "5d",
+     options:["Hyper Tube Markup Language", 
+     "Hyper Text Markup Language",
+     "Hypno-Tron Meerkat Lounge",
+     "Hold That Milk Log"], 
+     answer:"Hold That Milk Log"
  }
 ]
 
 //runs when start button is clicked and timer is set
 //this function will get an array index from var questions to be put into currentQuestion
-// function questionGen() {
-//     questionContent = 
-//     // currentQuestionInd = questions
-//     // [Math.floor(Math.random() * questions.length)];
-//     // console.log(currentQuestionInd);
-// }
+function clickQuestion() {
+    //check if its wrong
+    if (this.value !== questionSequence[currentQuestionInd].answer) {
+    timeLeft -= 5
+    console.log("you got it wrong")
+    }
+    currentQuestionInd++
+    if (currentQuestionInd === questionSequence.length){
+        alert("You've run out of questions")  //this will actually be a end of game function.
+    } else {
+        currentQuestion();
+    }
+}
+
+// answerSelect.onclick = clickQuestion
 
 //invoke by start btn. should display question and answer
 function currentQuestion(){
-    let i = 0;
-    document.getElementById("questioncontent").innerHTML = questionSequence[i].question; //get/display question content current index
-    document.getElementById("answer1").innerHTML = questionSequence[i].answerOne;
-    document.getElementById("answer2").innerHTML = questionSequence[i].answerTwo;
-    document.getElementById("answer3").innerHTML = questionSequence[i].answerThree;
-    document.getElementById("answer4").innerHTML = questionSequence[i].answerFour;
+    var currentQuestion = questionSequence[currentQuestionInd];
+    questionContent.textContent = currentQuestion.question;
+    answerSelect.innerHTML = "";
+    // let i = 0;
+    // document.getElementById("questioncontent").innerHTML = questionSequence[currentQuestionInd].question; //get/display question content current index
+    // document.getElementById("answer1").innerHTML = questionSequence[currentQuestionInd].answerOne;
+    // document.getElementById("answer2").innerHTML = questionSequence[currentQuestionInd].answerTwo;
+    // document.getElementById("answer3").innerHTML = questionSequence[currentQuestionInd].answerThree;
+    // document.getElementById("answer4").innerHTML = questionSequence[currentQuestionInd].answerFour;
+    currentQuestion.options.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice")
+        choiceNode.setAttribute("value", choice);
+        choiceNode.textContent = i + 1 + "." + choice;
+        choiceNode.onclick = clickQuestion;
+        answerSelect.appendChild(choiceNode)
+        
+    });
 
     //currently only working for first button 
-    answerSelect.addEventListener("click",()=> {
-        i++
-        document.getElementById("questioncontent").innerHTML = questionSequence[i].question; //get/display question content current index
-        document.getElementById("answer1").innerHTML = questionSequence[i].answerOne;
-        document.getElementById("answer2").innerHTML = questionSequence[i].answerTwo;
-        document.getElementById("answer3").innerHTML = questionSequence[i].answerThree;
-        document.getElementById("answer4").innerHTML = questionSequence[i].answerFour;
-    });
+    // answerSelect.onclick = clickQuestion
+    console.log(answerSelect);
+        
+        // if ()
+        // document.getElementById("questioncontent").innerHTML = questionSequence[currentQuestionInd].question; //displays question/answer content of next index
+        // document.getElementById("answer1").innerHTML = questionSequence[currentQuestionInd].answerOne;
+        // document.getElementById("answer2").innerHTML = questionSequence[currentQuestionInd].answerTwo;
+        // document.getElementById("answer3").innerHTML = questionSequence[currentQuestionInd].answerThree;
+        // document.getElementById("answer4").innerHTML = questionSequence[currentQuestionInd].answerFour;
     //need something for when i > questions.length
 }
 
-function selectAnswer(event) {
-    answerSelect.addEventListener("click",()=> {
-        i++
+// function selectAnswer(event) {
+//     answerSelect.addEventListener("click",()=> {
+//         i++
     
-    });
-}
+//     });
+// }
 
 
 
