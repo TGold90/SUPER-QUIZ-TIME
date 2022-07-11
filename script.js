f70390909ceca3f3bc851984f0d43965a8700558
TGOLD <thomaswgold90@gmail.com>
2022-07-11 15:14:07 -0600

    minor changes, cleaning up old stuff

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-//PSEUDO CODE
-//create start button that init timer funct and quiz questions funct
-//quiz question answer input must generate into next question
-//incorrect answer = -time
-//timer=0 or all questions answered --> game is over
-//input for initials and final score shown. can be saved.
-
-//Credits: got some of my iteration ideas from hamdan on stackoverflow
-
-
-    
+  
 var startBtn = document.querySelector("#startbtn"); //start button
 var timerElement = document.querySelector("#timer"); //timer content
-// var quizBox = document.querySelector("#quizBox");
 var answerSelect = document.querySelector(".buttonbox"); //all answer buttons - click event
-var decrement = document.querySelector("#decrement");
 var questionContent = document.querySelector("#questioncontent");//p tag - should display the actual question
 var scoreList = document.querySelector(".highscoreList");
 var highscoreBox = document.querySelector(".highscores")
 var highscoreBtn = document.querySelector("#HSbutton");
 
-// var resetBtn = document.querySelector()
 var timer;
 var timeLeft = 45;
 var currentQuestionInd = 0;
@@ -104,7 +91,7 @@ function clickQuestion() {
     }
 }
 
-function endOfGame(){
+function endOfGame(){ //invoked when time runs out or last question answered. creates form with input to submit highscore.
     answerSelect.innerHTML = "Score: " + timeLeft;
     questionContent.textContent = "You've Reached the end of the game!";
     var highscoreForm = document.createElement("form");
@@ -117,7 +104,7 @@ function endOfGame(){
     highscoreForm.appendChild(inputLabel);
     highscoreForm.appendChild(highscoreInput);
 
-    highscoreForm.addEventListener("submit", function(event) { //this function is basically borrowed exactly from activity 26 in module 4
+    highscoreForm.addEventListener("submit", function(event) { //this function is borrowed from activity 26 in module 4
         event.preventDefault();
       
         var scoreText = highscoreInput.value.trim();
@@ -128,22 +115,14 @@ function endOfGame(){
 
         scores.push(scoreText + ": " + timeLeft);
         highscoreInput.value = "";
-        //here there should be a function to save to local storage
-        //then reset webpage
-        //at webpage init the scores will be grabbed from local storage
-        // highscoreInput.value = "";
-        
-          // Store updated todos in localStorage, re-render the list
         storeScores();
         showScores();
+        location.reload();
     });
 }
 
 function showScores() {
-    // Clear todoList element and update todoCountSpan
-    scoreList.innerHTML = "";
-  
-    // Render a new li for each todo
+
     for (var i = 0; i < scores.length; i++) {
       var score = scores[i];
   
@@ -156,23 +135,20 @@ function showScores() {
 }
 
 function openPage() { 
-    // Get stored todos from localStorage
     var storedScores = JSON.parse(localStorage.getItem("scores"));
   
-    // If todos were retrieved from localStorage, update the todos array to it
     if (storedScores !== null) {
       scores = storedScores;
     }
   
-    // This is a helper function that will render todos to the DOM
     showScores();
   }
 
+  //adds score to local storage scores array
 function storeScores() {
-    // Stringify and set key in localStorage to todos array
     localStorage.setItem("scores", JSON.stringify(scores));
 }
-// answerSelect.onclick = clickQuestion
+
 
 //invoke by start btn. should display question and answer
 function currentQuestion(){
@@ -194,12 +170,9 @@ function currentQuestion(){
     console.log(answerSelect);
 }
 
-function getScores(){
-
-};
-
 openPage();
 
+//Mouseenter/mouseleave function to show/hide highscores
 highscoreBtn.addEventListener("mouseover", function() {
  highscoreBox.style.display = "block";
  highscoreBtn.addEventListener("mouseleave", function(){
